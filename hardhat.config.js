d537aa57bd30136f1c39ec341115cce8e16a6827
flyinglimao <hi@limaois.me>
2022-08-19 01:43:13 +0800

    init

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+require('@nomicfoundation/hardhat-toolbox');
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: '0.8.16',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.RPC_URL,
+      },
+    },
+  },
+};
